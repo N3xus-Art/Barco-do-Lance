881b5f2c6c3a97e6fde44ce5328851a6a83f966f
jprvivasGS <jprvivas@gmail.com>
2025-04-10 16:01:11 -0300

    FEAT: scripts de movimentação

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,12 +5,17 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        Vector2 pos = new Vector2(3, 1);
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.Translate(Vector2.right * Time.deltaTime * 3);
+
     }
 }
