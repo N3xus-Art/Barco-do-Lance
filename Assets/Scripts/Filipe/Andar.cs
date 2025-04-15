acb297cdf28b8c2b8f04ad394536fade6e99f3a4
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-04-15 16:01:28 -0300

    FEAT: EU DOU A BUNDA PARA VÁRIOS HOMENS E ESTOU BEBADO POR ISSO ESTOU ESCREVENDO ISSO SÓ PRA DAR UM FEAT NUM APRENDIZADO DESSA MERDA DE UNITY

---
+++ Assets/Scripts/Filipe/Andar.cs
@@ -0,0 +1,17 @@
+ using UnityEngine;
+ using UnityEngine.InputSystem;
+
+public class Andar : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      Debug.Log("Começou essa bosta foda");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
