3d38346c3a53b83e859d2517f125e43fcef20f47
jprvivasGS <jprvivas@gmail.com>
2025-04-16 16:33:44 -0300

    FEAT: todos os botões do jogo
    :D

--- Assets/Scripts/Jp/PlayerMovementHandler.cs
+++ Assets/Scripts/Jp/PlayerMovementHandler.cs
@@ -1,18 +1,58 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovementHandler : MonoBehaviour
 {
+    [SerializeField] float velocity;
+    bool isMoving;
+    bool isInteracting;
+    Vector2 direction;
+
 
     //Classe Move que contém a movimentação do jogador
     public void Move(InputAction.CallbackContext context){
+        
+        if(context.phase == InputActionPhase.Started){
+           
+        }else if(context.phase == InputActionPhase.Performed){
+            isMoving = true;
+            direction = context.ReadValue<Vector2>();
+            
+        
+        }else if(context.phase == InputActionPhase.Canceled){
+            isMoving = false;       
+        
+        }
+    }
 
-        if(context.phase == InputActionPhase.Performed){
-            Debug.Log("Move");
+    public void Interact(InputAction.CallbackContext context){
+
+        if(context.phase == InputActionPhase.Started){
+           
+        }else if(context.phase == InputActionPhase.Performed){
+            isInteracting = true;
+            
+        }else if(context.phase == InputActionPhase.Canceled){
+            isInteracting = false;       
+        }
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * velocity;
         }
         
-        
+        if(isInteracting){
+            Debug.Log("Foi");
+
+        }
 
     }
+
 
 }
