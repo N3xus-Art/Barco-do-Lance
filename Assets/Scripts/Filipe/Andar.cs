802a12f9805bf0efcd29bfb71683cc654e540665
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-04-16 16:05:06 -0300

    Feat: Mais estudo

--- Assets/Scripts/Filipe/Andar.cs
+++ Assets/Scripts/Filipe/Andar.cs
@@ -3,15 +3,14 @@
 
 public class Andar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-      Debug.Log("Começou essa bosta foda");  
-    }
+
+  [SerializeField] float velocity;
 
     // Update is called once per frame
     void Update()
     {
         
+       transform.Translate(Vector2.right * Time.deltaTime * velocity); 
+
     }
 }
