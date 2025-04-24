d5d89570f1c8f531ddac23861e60b74e183c0aad
FilipeM-Azevedo <filipem.azevedo.pro@gmail.com>
2025-04-24 16:00:15 -0300

    Feat: Canva e Teste de Hud
    Teste das huds e o Canva do menu principal

---
+++ Assets/Scripts/Filipe/ScriptMenuPrincipal.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScriptMenuPrincipal : MonoBehaviour
+{
+    public void NovoJogo()
+    {
+        Debug.Log("Novo Jogo");
+
+    }
+
+    public void Opcoes()
+    {
+
+        Debug.Log("Opções");
+
+    }
+
+    public void Sair()
+    {
+
+        Debug.Log("Sair");
+
+    }
+
+}
+
---
+++ Assets/Scripts/Filipe/ScriptTesteHud.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class ScriptTesteHud : MonoBehaviour
+{
+    
+    [SerializeField] public int Vida;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
