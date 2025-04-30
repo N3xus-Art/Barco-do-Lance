2dac80fe46cac9366e6fe3a94ad0c9cceebf4395
jprvivasGS <jprvivas@gmail.com>
2025-04-30 14:21:39 -0300

    FEAT: o jogador morre

--- Assets/Scripts/Jp/PlayerHandler.cs
+++ Assets/Scripts/Jp/PlayerHandler.cs
@@ -4,8 +4,17 @@ using UnityEngine.UI;
 using System;
 
 public class PlayerHandler : MonoBehaviour{
-    public int o2;
+    public Image BarraOxigenio;
+    public float o2, MaxOxygen, OxygenCost;
     void Update(){
+
+        o2 = o2 - OxygenCost;
+        BarraOxigenio.fillAmount = o2 / MaxOxygen; 
+
+        if (o2 < 0) {
+            o2 = 0;
+            Destroy(gameObject);
+        }
 
     }
 }
