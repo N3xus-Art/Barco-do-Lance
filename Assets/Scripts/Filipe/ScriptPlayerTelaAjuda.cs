b1d8213fd76a8f1077cef3d71b55054e79c9440d
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-05-06 16:33:14 -0300

    FEAT: essa desgraça foi um cu de resolver mas eu i jp resolvemos essa merda

--- Assets/Scripts/Filipe/ScriptPlayerTelaAjuda.cs
+++ Assets/Scripts/Filipe/ScriptPlayerTelaAjuda.cs
@@ -6,26 +6,61 @@ using UnityEngine.UI;
 public class ScriptPlayerTelaAjuda : MonoBehaviour
 {
     public Canvas TelaAjuda;
+    public Transform Image;
+
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+
+    public int numBotoes = 0;
 
     public void HabilitarCanva(InputAction.CallbackContext context)
     {
         
         if(context.phase == InputActionPhase.Started){
 
-            if (TelaAjuda != null)
-                {
+            if (TelaAjuda != null && TelaAjuda.enabled == false){
+                    Debug.Log("doce");
 
-                    if (TelaAjuda.enabled == false){TelaAjuda.enabled = true;}else{TelaAjuda.enabled = false;}
+                    button1.SetActive(true);
+                    button2.SetActive(true);
+                    button3.SetActive(true);
+                    button4.SetActive(true);
+                    button5.SetActive(true);
+                    button6.SetActive(true);
 
-                }
+                    if (Image != null)
+                    {
 
+                        foreach (Transform child in Image)
+                        {
+                            Button button = child.GetComponent<Button>();
+
+                            if (button != null)
+                            {
+                                numBotoes++;
+                            }
+                        }
+                    }
+
+                    TelaAjuda.enabled = true;
             }
+        }
+    }
+
+    void Start()
+    {
 
     }
 
 
     void Update()
-    {
+    {   
+
+        if (numBotoes == 0){ TelaAjuda.enabled = false; };
 
     }
 
