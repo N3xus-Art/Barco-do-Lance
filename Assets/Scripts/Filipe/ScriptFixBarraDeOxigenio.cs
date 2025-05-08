3d50ec38d1ffacaee164ddee7d664c935b4f1dea
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-05-08 13:52:51 -0300

    Perf: Melhorei o código da Barra de Oxigenio

---
+++ Assets/Scripts/Filipe/ScriptFixBarraDeOxigenio.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptFixBarraDeOxigenio : MonoBehaviour
+{
+   public PlayerHandler Player;
+   public Image BarraOxigenio;
+
+    void Update()
+    {
+
+      BarraOxigenio.fillAmount = Player.o2 / Player.MaxOxygen; 
+      
+    }
+
+}
