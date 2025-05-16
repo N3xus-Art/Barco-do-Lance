a643c73da778a9c65f74113a057a53c2854c70cf
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-05-16 11:41:30 -0300

    FEAT: Mais coisa da Build1 (Scene Load, Menus Iniciais)

---
+++ Assets/Scripts/Filipe/Menus Iniciais/MenuController.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using System;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerFilipe : MonoBehaviour
+{
+    SceneController sceneController;
+    public void NewGame()
+    {
+        sceneController = new SceneController();
+        sceneController.LoadScene("Cena Navegacao");
+
+
+    }
+
+    public void Config()
+    {
+
+        sceneController = new SceneController();
+        sceneController.LoadScene("Menu Configuracoes");
+
+    }
+
+    public void Credits()
+    {
+
+        sceneController = new SceneController();
+        sceneController.LoadScene("Creditos");
+
+    }
+
+    public void Back()
+    {
+        
+        sceneController = new SceneController();
+        sceneController.LoadScene("Cena Tela Inicial");
+
+    }
+
+}
