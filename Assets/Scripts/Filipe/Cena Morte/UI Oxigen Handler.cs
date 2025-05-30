05bf07877e3ba261f19bf2b6688ee9d2e35d5d75
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-05-30 10:55:03 -0300

    FEAT: Morte do Personagem

--- Assets/Scripts/Filipe/Cena Morte/UI Oxigen Handler.cs
+++ Assets/Scripts/Filipe/Cena Morte/UI Oxigen Handler.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.InputSystem.Controls;
 using UnityEngine.UI;
+
 public class UIOxigenHandler : MonoBehaviour
 {
     public PlayerHandlerDeath Player;
+
+    public GameObject GO_Player;
     public Image BarraOxigenio;
     public float FadeTime;
+    public GameObject TextoMorte;
+    public float TimerDeath = 0;
     public CanvasGroup FadeImage;
 
     public void Fade(bool FadeAway = true)
@@ -24,7 +26,7 @@ public class UIOxigenHandler : MonoBehaviour
         else
         {
          
-          if (FadeImage.alpha > 1)
+          if (FadeImage.alpha > 0)
             {
 
                 FadeImage.alpha -= FadeTime * Time.deltaTime;
@@ -42,15 +44,40 @@ public class UIOxigenHandler : MonoBehaviour
 
         if (Player.o2 <= 0)
         {
-            if (FadeImage.alpha < 1)
+            if (FadeImage.alpha < 1 && Player.Morreu == false)
             {
-                Debug.Log("Sem Oxigênio");
                 Fade(true);
+
+                if (FadeImage.alpha >= 1) { Player.Morreu = true; TimerDeath = 5; }
+            }
+
+
+            if (TimerDeath > 0)
+            {
+                TimerDeath -= Time.deltaTime;
+
+                if (TextoMorte.activeSelf == false && TimerDeath < 4.5) { TextoMorte.SetActive(true); }
+
+                if (TimerDeath <= 0) { TextoMorte.SetActive(false); }
             }
             else
             {
-                Fade(false);
+
+                if (Player.Morreu == true)
+                {
+
+                    if (FadeImage.alpha > 0){
+
+                        Fade(false);
+                        GO_Player.transform.localPosition = new Vector3(0, 0, 0);
+
+                    }else { Player.Morreu = false; Player.o2 = Player.MaxOxygen; }
+
+                }
+
             }
+
+
 
         }
 
