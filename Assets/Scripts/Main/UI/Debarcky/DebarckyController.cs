24933ec782533095636416eed87c0615ff05765c
jprvivasGS <62661171+jprvivasGS@users.noreply.github.com>
2025-06-03 15:52:35 -0300

    FEAT: Ultimo commit preferias

--- Assets/Scripts/Main/UI/Debarcky/DebarckyController.cs
+++ Assets/Scripts/Main/UI/Debarcky/DebarckyController.cs
@@ -5,22 +5,22 @@ using UnityEngine.SceneManagement;
 public class DebarckyController : MonoBehaviour
 {
     SceneController sceneController;
-    public void Medi()
-    {
+    public void Medi(){
         sceneController = new SceneController();
         sceneController.LoadScene("Mediterranean Sea");
     }
 
-    public void Cari()
-    {
-    
+    public void Cari(){
         sceneController = new SceneController();
         sceneController.LoadScene("Caribbean Sea");
     }
-    public void Red()
-    {
-        Scene activeScene = SceneManager.GetActiveScene();
+    public void Red(){
         sceneController = new SceneController();
         sceneController.LoadScene("Red Sea");
+    }
+
+    public void Boat(){
+        sceneController = new SceneController();
+        sceneController.LoadScene("Scene Navigation");
     }
 }
