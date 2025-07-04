d51decb430cb959f1c2e1b80d55706b118ce3859
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-07-04 11:36:45 -0300

    FEAT: Scroll do menu

--- Assets/Scripts/Filipe/Menu Configuracoes/Mute Handler.cs
+++ Assets/Scripts/Filipe/Menu Configuracoes/Mute Handler.cs
@@ -1,13 +1,25 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MuteHandler : MonoBehaviour
 {
     public OptionsHandler OptionsHandler;
+    public UnityEngine.UI.Image Botao;
+    public Sprite SpriteDesmutado;
+    public Sprite SpriteMutado;
+
 
     public void Click()
     {
 
         OptionsHandler.Ligado ^= true;
 
+        if(Botao.sprite == SpriteDesmutado) { Botao.sprite = SpriteMutado; } else { Botao.sprite = SpriteDesmutado; }
+    
     }
+
+
+
 }
