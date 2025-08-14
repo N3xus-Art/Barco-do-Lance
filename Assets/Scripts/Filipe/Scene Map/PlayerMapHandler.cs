b4298921250b0a961a8a80f48486447e6a9ff2b0
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-08-13 22:46:50 -0300

    FEAT: Parte Estrutural da Cena de Navegação TopDown

--- Assets/Scripts/Filipe/Scene Map/PlayerMapHandler.cs
+++ Assets/Scripts/Filipe/Scene Map/PlayerMapHandler.cs
@@ -1,11 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerMapHandler : MonoBehaviour
 {
 
+    string Index = "";
+
+    SceneController sceneController;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.gameObject);    
+
+        Index = other.name;
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+
+        Index = "";
+
+    }
+
+    public void ChangeScene(InputAction.CallbackContext context) 
+    {
+
+       if (context.phase == InputActionPhase.Started) 
+       {
+        
+            if (Index != "") {
+
+                sceneController = new SceneController();
+                sceneController.LoadScene(Index, LoadSceneMode.Additive);
+
+            }
+            
+        
+       }
+    
     }
 
 }
