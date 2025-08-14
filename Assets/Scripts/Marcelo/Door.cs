e8765bdc6377daa4d7cbd6e3361f860d492a2830
Marcelo Fernandes <132860265+Marcelo-122@users.noreply.github.com>
2025-08-14 15:51:30 -0300

    Testes com a verificação TemTanque

---
+++ Assets/Scripts/Marcelo/Door.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public Player target;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Alguma coisa acontece
+        if (other.gameObject.GetComponent<Player>().temTanque)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Marcelo/Player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Verifica se o player tem um tanque de O2 equipado
+    public bool temTanque = true;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
