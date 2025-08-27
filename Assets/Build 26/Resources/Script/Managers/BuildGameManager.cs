59db89af98b760c209e863718734a55d0e7c78e8
jprvivasGS <jprvivas@gmail.com>
2025-08-27 05:25:35 -0300

    FEAT: ultimo commit antes da extensão
    não deu pra por tudo ainda
     :/

--- Assets/Build 26/Resources/Script/Managers/BuildGameManager.cs
+++ Assets/Build 26/Resources/Script/Managers/BuildGameManager.cs
@@ -9,15 +9,17 @@ public class BuildGameManager : MonoBehaviour {
     //Variables
     #region
     static BuildGameManager Instance;
+    [SerializeField] private bool isPaused;
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject confirmExit;
     [SerializeField] private GameObject buttons;
-    [SerializeField] private bool isPaused;
     [SerializeField] private SceneHandler sceneHandler;
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button configButton;
     [SerializeField] private Button homeButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private Button backButton;
     #endregion
 
     //Methods
@@ -29,6 +31,18 @@ public class BuildGameManager : MonoBehaviour {
         } else if (Instance != this) {
             Destroy(gameObject);
         }
+        Button resume = resumeButton.GetComponent<Button>();
+        Button config = configButton.GetComponent<Button>();
+        Button home = homeButton.GetComponent<Button>();
+        Button quit = quitButton.GetComponent<Button>();
+        Button confirm = confirmExit.GetComponent<Button>();
+        Button back = backButton.GetComponent<Button>();
+        resume.onClick.AddListener(ResumeGame);
+        config.onClick.AddListener(Config);
+        home.onClick.AddListener(Home);
+        quit.onClick.AddListener(Confirm);
+        //confirm.onClick.AddListener(Quit);
+        back.onClick.AddListener(Back);
     }
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -54,22 +68,20 @@ public class BuildGameManager : MonoBehaviour {
         sceneHandler = new SceneHandler();
         sceneHandler.LoadScene("ConfigurationsScreen", LoadSceneMode.Additive);
     }
-    public void Back(){
+    public void Home(){
         sceneHandler = new SceneHandler();
         sceneHandler.LoadScene("HomeScreen");
     }
-    public void Show(){
+    public void Confirm(){
         confirmExit.SetActive(true);
         buttons.SetActive(false);
     }
-    public void Hide(){
+    public void Back(){
         confirmExit.SetActive(false);
         buttons.SetActive(true);
     }
     public void Quit(){
         Application.Quit();
     }
-
     #endregion
-
 }
