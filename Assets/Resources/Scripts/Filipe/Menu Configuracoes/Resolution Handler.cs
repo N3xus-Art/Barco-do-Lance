6a3cf9f853c4224dd07a14bf89c12baad820ef96
jprvivasGS <62661171+jprvivasGS@users.noreply.github.com>
2025-08-27 14:09:49 -0300

    esse commit não existe

--- Assets/Resources/Scripts/Filipe/Menu Configuracoes/Resolution Handler.cs
+++ Assets/Resources/Scripts/Filipe/Menu Configuracoes/Resolution Handler.cs
@@ -5,9 +5,9 @@ using TMPro;
 public class ResolutionHandler : MonoBehaviour
 {
 
-    public string[] Resolucoes    = { "1920x1080","1600x1200", "1x1" };
-    public int[] ResolucoesWidth  = { 1920,1600, 1 };
-    public int[] ResolucoesHeight = { 1080,1200, 1 };
+    public string[] Resolucoes    = { "1920x1080","1600x1200", "1000x1000" };
+    public int[] ResolucoesWidth  = { 1920,1600, 1000 };
+    public int[] ResolucoesHeight = { 1080,1200, 1000 };
 
     public int Index = 0;
     public int MaxIndex;
