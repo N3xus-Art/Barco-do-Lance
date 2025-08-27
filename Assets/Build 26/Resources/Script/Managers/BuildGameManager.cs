a4b80412fd5ad5b3075b405cce58246bcbb0b06f
jprvivasGS <jprvivas@gmail.com>
2025-08-27 05:05:38 -0300

    Feat: Primeira parte do menu de pause

--- Assets/Build 26/Resources/Script/Managers/BuildGameManager.cs
+++ Assets/Build 26/Resources/Script/Managers/BuildGameManager.cs
@@ -1,23 +1,75 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class BuildGameManager : MonoBehaviour{
+public class BuildGameManager : MonoBehaviour {
     //Variables
     #region
     static BuildGameManager Instance;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject confirmExit;
+    [SerializeField] private GameObject buttons;
+    [SerializeField] private bool isPaused;
+    [SerializeField] private SceneHandler sceneHandler;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button configButton;
+    [SerializeField] private Button homeButton;
+    [SerializeField] private Button quitButton;
     #endregion
 
     //Methods
     #region
-    void Awake(){
-        if (Instance == null){
+    void Awake() {
+        if (Instance == null) {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        }else if(Instance != this){
+        } else if (Instance != this) {
             Destroy(gameObject);
         }
     }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused) {
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Config (){
+        sceneHandler = new SceneHandler();
+        sceneHandler.LoadScene("ConfigurationsScreen", LoadSceneMode.Additive);
+    }
+    public void Back(){
+        sceneHandler = new SceneHandler();
+        sceneHandler.LoadScene("HomeScreen");
+    }
+    public void Show(){
+        confirmExit.SetActive(true);
+        buttons.SetActive(false);
+    }
+    public void Hide(){
+        confirmExit.SetActive(false);
+        buttons.SetActive(true);
+    }
+    public void Quit(){
+        Application.Quit();
+    }
+
     #endregion
 
 }
