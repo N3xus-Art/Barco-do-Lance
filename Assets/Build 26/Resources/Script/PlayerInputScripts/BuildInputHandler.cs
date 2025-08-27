9da21a0c26ad25e708ff7185271bbafa75958473
jprvivasGS <jprvivas@gmail.com>
2025-08-26 23:56:10 -0300

    FEAT: novo sistema de movimentação

--- Assets/Build 26/Resources/Script/PlayerInputScripts/BuildInputHandler.cs
+++ Assets/Build 26/Resources/Script/PlayerInputScripts/BuildInputHandler.cs
@@ -9,18 +9,30 @@ using UnityEngine.SceneManagement;
 public class BuildInputHandler : MonoBehaviour{
     //Variables
     #region
-    [SerializeField] private bool isMoving, isInteracting, isClicking;
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private Vector2 direction;
+    [Header("----Boleanas----")]
+    [SerializeField] private bool isMoving;
+    [SerializeField] private bool isInteracting;
+    [SerializeField] private bool isClicking;
+    [Header("----Variaveis de Movimentação----")]
+    [SerializeField] private float speed;
+    [SerializeField] private float horizontalMoviment;
+    [SerializeField] private float verticalMoviment;
+    [SerializeField] private Rigidbody2D rb;
+    [Header("----Verificação de Contato----")]
+    [SerializeField] private Transform contactCheckPos;
+    [SerializeField] private Vector2 contactCheckSize = new Vector2(0.13f, 0.05f);
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask waterLayer;
     #endregion
 
-    //Methods
+    //Input Methdos
     #region
     public void Move(InputAction.CallbackContext context){
         if(context.phase == InputActionPhase.Started){
         }else if (context.phase == InputActionPhase.Performed){
             isMoving = true;
-            direction = context.ReadValue<Vector2>();
+            horizontalMoviment = context.ReadValue<Vector2>().x;
+            verticalMoviment = context.ReadValue<Vector2>().y;
         }else if(context.phase == InputActionPhase.Canceled){
             isMoving = false;
         }
@@ -41,11 +53,39 @@ public class BuildInputHandler : MonoBehaviour{
             isClicking = false;
         }
     }
+    #endregion
+    //ContactCheck Methods
+    #region
+    private void OnDrawGizmosSelected(){
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(contactCheckPos.position, contactCheckSize);
+    }
+    private bool isGrounded(){
+        if (Physics2D.OverlapBox(contactCheckPos.position, contactCheckSize, 0, groundLayer)){
+            return true;
+        }
+        return false;
+    }
+    private bool inWater(){
+        if (Physics2D.OverlapBox(contactCheckPos.position, contactCheckSize, 0, waterLayer)){
+            return true;
+        }
+        return false;
+    }
+    #endregion
+    //Unity Methods
+    #region
+    public void Awake(){
+        speed = 5f;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
     public void Update(){
-        if (isMoving){
-            transform.position += new Vector3(direction.x, 0, 0) * Time.deltaTime * speed;
-        }else if (isMoving && WaterHandler.onWater){
-                transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        if (isMoving && isGrounded()){
+            rb.linearVelocity = new Vector2(horizontalMoviment * speed, 0);
+        }else if (isMoving && inWater()){
+            rb.linearVelocity = new Vector2(horizontalMoviment * speed, verticalMoviment * speed);
+        }else if (!isMoving){
+            rb.linearVelocity = new Vector2(0, 0);
         }
     }
     #endregion
