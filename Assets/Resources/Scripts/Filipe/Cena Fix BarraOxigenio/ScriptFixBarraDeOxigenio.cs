0519cda2353730f9b6a2e22ec211d4db2272478b
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-09-02 15:55:37 -0300

    FEAT: Nova Hud do Oxigenio

--- Assets/Resources/Scripts/Filipe/Cena Fix BarraOxigenio/ScriptFixBarraDeOxigenio.cs
+++ Assets/Resources/Scripts/Filipe/Cena Fix BarraOxigenio/ScriptFixBarraDeOxigenio.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.UI;
 
 public class ScriptFixBarraDeOxigenio : MonoBehaviour
 {
-   public PlayerHandler Player;
-   public Image BarraOxigenio;
+
+    [SerializeField]
+    float rotation; 
+    
+    public float o2 = 100; int MaxO2 = 100;
+
 
     void Update()
     {
 
-      //BarraOxigenio.fillAmount = Player.o2 / Player.MaxOxygen; 
-      
+        if (o2 > 0) { o2 -= 1 * Time.deltaTime; }    
+
+        rotation = -160f + (o2/MaxO2) * (160f - (-160f));
+
+        // tank full oxigen 160, zero oxigen -160
+        gameObject.GetComponent<RectTransform>().rotation = Quaternion.Euler(0f, 0f, -rotation);
+
+
     }
 
 }
