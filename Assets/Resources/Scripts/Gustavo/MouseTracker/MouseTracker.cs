597d6653c8098a10020039c7ce4f5c64f1b097c6
Gustavo Santos <faceman8456@gmail.com>
2025-09-03 15:57:30 -0300

    Tentativa de criação da tela de ajuda

---
+++ Assets/Resources/Scripts/Gustavo/MouseTracker/MouseTracker.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour
+{
+    void Update()
+    {
+       /* // Get the mouse position in screen coordinates
+        Vector3 mouseScreenPosition = Input.mousePosition;
+
+        // Convert the screen position to world coordinates
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(
+            mouseScreenPosition.x,
+            mouseScreenPosition.y,
+            Camera.main.nearClipPlane // Distance from the camera to the near clipping plane
+        ));
+
+        // Log the position to the console
+        Debug.Log("Mouse Position (World): " + mouseWorldPosition); */
+    }
+}
