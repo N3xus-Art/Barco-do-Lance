86f92a3da4f2a8371e81b9e3f5d373d18e023421
Marcelo Fernandes <132860265+Marcelo-122@users.noreply.github.com>
2025-09-03 15:50:06 -0300

    wip - mudanças na logica do inventory

--- Assets/Resources/Scripts/Marcelo/ToolUi.cs
+++ Assets/Resources/Scripts/Marcelo/ToolUi.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using System;
 
 public class EquippedToolUI : MonoBehaviour
 {
@@ -9,23 +10,32 @@ public class EquippedToolUI : MonoBehaviour
     private void OnEnable()
     {
         if (playerInventory != null)
+        {
+            playerInventory.OnInventoryChanged += UpdateToolText;
             playerInventory.OnEquippedToolChanged += UpdateToolText;
+        }
+
+            
     }
 
     private void OnDisable()
     {
         if (playerInventory != null)
-            playerInventory.OnEquippedToolChanged -= UpdateToolText;
+        {
+            playerInventory.OnInventoryChanged += UpdateToolText;
+            playerInventory.OnEquippedToolChanged += UpdateToolText;
+        }
     }
 
     private void Start()
     {
-        UpdateToolText(playerInventory.EquippedTool);
+        UpdateToolText();
     }
+
 
     private void UpdateToolText(ToolInstance tool)
     {
-        if (tool != null)
+        if (playerInventory.EquippedTool != null)
         {
             ToolText.text = $"Ferramenta atual: {tool.Data.toolName} ({tool.CurrentDurability}/{tool.MaxDurability})";
         }
@@ -34,4 +44,15 @@ public class EquippedToolUI : MonoBehaviour
             ToolText.text = "Ferramenta atual: Nenhuma";
         }
     }
+    private void UpdateToolText()
+    {
+        if (playerInventory.EquippedTool != null)
+        {
+            ToolText.text = $"Ferramenta atual: {playerInventory.EquippedTool.Data.toolName} ({playerInventory.EquippedTool.CurrentDurability}/{playerInventory.EquippedTool.MaxDurability})";
+        }
+        else
+        {
+            ToolText.text = "Ferramenta atual: Nenhuma";
+        }
+}
 }
