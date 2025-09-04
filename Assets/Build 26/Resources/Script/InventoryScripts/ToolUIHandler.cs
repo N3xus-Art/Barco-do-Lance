f0b178669dbcacaa2f19e92f9e0d0d83970b8abe
jprvivasGS <jprvivas@gmail.com>
2025-09-04 04:02:48 -0300

    FEAT?: eu não consegui incluir as coisas de inventario, mas meio que tá lá

---
+++ Assets/Build 26/Resources/Script/InventoryScripts/ToolUIHandler.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+public class EquippedToolUIHandler : MonoBehaviour
+{
+    public PlayerInventory playerInventory;
+    public TextMeshProUGUI ToolText;
+
+    private void OnEnable()
+    {
+        if (playerInventory != null)
+        {
+            playerInventory.OnInventoryChanged += UpdateToolText;
+            playerInventory.OnEquippedToolChanged += UpdateToolText;
+        }
+
+
+    }
+
+    private void OnDisable()
+    {
+        if (playerInventory != null)
+        {
+            playerInventory.OnInventoryChanged += UpdateToolText;
+            playerInventory.OnEquippedToolChanged += UpdateToolText;
+        }
+    }
+
+    private void Start()
+    {
+        UpdateToolText();
+    }
+
+
+    private void UpdateToolText(ToolInstance tool)
+    {
+        if (playerInventory.EquippedTool != null)
+        {
+            ToolText.text = $"Ferramenta atual: {tool.Data.toolName} ({tool.CurrentDurability}/{tool.MaxDurability})";
+        }
+        else
+        {
+            ToolText.text = "Ferramenta atual: Nenhuma";
+        }
+    }
+    private void UpdateToolText()
+    {
+        if (playerInventory.EquippedTool != null)
+        {
+            ToolText.text = $"Ferramenta atual: {playerInventory.EquippedTool.Data.toolName} ({playerInventory.EquippedTool.CurrentDurability}/{playerInventory.EquippedTool.MaxDurability})";
+        }
+        else
+        {
+            ToolText.text = "Ferramenta atual: Nenhuma";
+        }
+    }
+}
