7e0547764d7889b6dc14ba5b3ac69c6c59429038
Gustavo Santos <faceman8456@gmail.com>
2025-09-18 15:06:35 -0300

    Feat - prefabs
    Prefabs da CapturaUI e da OperacaoUI

--- Assets/Resources/Scripts/Gustavo/Animal/Animais/AnimalSpawner.cs
+++ Assets/Resources/Scripts/Gustavo/Animal/Animais/AnimalSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AnimalSpawner : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    public Transform AreaSpawn;
     public float minX = -8f;
     public float maxX = 8f;
     public float minY = -4f;
@@ -21,9 +22,13 @@ public class AnimalSpawner : MonoBehaviour
     {
         // Escolhe um prefab aleatoriamente do array
         GameObject animalToSpawn = animalPrefabs[Random.Range(0, animalPrefabs.Length)];
+        RectTransform area = AreaSpawn.GetComponent<RectTransform>();
 
         // Gera uma posição aleatória dentro da área definida
-        Vector2 spawnPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Vector2 spawnPosition = new Vector2(
+                Random.Range(-area.rect.width / 2, area.rect.width / 2),
+                Random.Range(-area.rect.height / 2, area.rect.height / 2)
+            );
 
         // Instancia o animal na posição aleatória
         Instantiate(animalToSpawn, spawnPosition, Quaternion.identity);
