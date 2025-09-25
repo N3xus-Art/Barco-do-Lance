bb91350a78e1effc310897a9719b029c0e5f230e
jprvivasGS <62661171+jprvivasGS@users.noreply.github.com>
2025-09-25 14:33:45 -0300

    FEAT: mapa das ilhas no fluxo do jogo

--- Assets/Build 26/Resources/Script/IslandScripts/IslandHandler.cs
+++ Assets/Build 26/Resources/Script/IslandScripts/IslandHandler.cs
@@ -15,18 +15,33 @@ public class IslandHandler : MonoBehaviour{
     [SerializeField] private GameObject confirmScreen;
     [SerializeField] private GameObject poorScreen;
     [SerializeField] private Button closeButton;
-    [Header("----Variaveis de Items----")]
     [SerializeField] private TMP_Text moneyText;
     [SerializeField] private int money;
-    [SerializeField] private int firstItemPrice;
-    [SerializeField] private int secondItemPrice;
-    [SerializeField] private int thirdItemPrice;
+    [SerializeField] private int itemSlot;
+    [Header("----Variaveis do primeiro item----")]
     [SerializeField] private string firstItemName;
+    [SerializeField] private int firstItemPrice;
+    [SerializeField] private GameObject firstGameObject;
+    [SerializeField] private TMP_Text firstItemText;
+    [SerializeField] private Sprite firstSprite;
+    [Header("----Variaveis do segundo item----")]
     [SerializeField] private string secondItemName;
+    [SerializeField] private int secondItemPrice;
+    [SerializeField] private GameObject secondGameObject;
+    [SerializeField] private TMP_Text secondItemText;
+    [SerializeField] private Sprite secondSprite;
+    [Header("----Variaveis do terceiro item----")]
     [SerializeField] private string thirdItemName;
+    [SerializeField] private int thirdItemPrice;
+    [SerializeField] private GameObject thirdGameObject;
+    [SerializeField] private TMP_Text thirdItemText;
+    [SerializeField] private Sprite thirdSprite;
+    [Header("----Variaveis dos Botões----")]
     [SerializeField] private Button firstButton;
     [SerializeField] private Button secoundButton;
     [SerializeField] private Button thirdButton;
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private Button denyButton;
     #endregion
     //Methods
     #region
@@ -37,6 +52,7 @@ public class IslandHandler : MonoBehaviour{
 
         void OnTriggerExit2D(Collider2D collider2D){
             ison = false;
+            BuildInputHandler.isInteracting = false;
         }
 
     //Button Methods
@@ -44,63 +60,76 @@ public class IslandHandler : MonoBehaviour{
             ison = false;
             shopGO.SetActive(false);
             Time.timeScale = 1;
+            BuildInputHandler.isInteracting = false;
         }
 
         //Confirms
-            public void ConfirmFirst() {
+            public void OnFirst() {
                 if (money < firstItemPrice){
                     poorScreen.SetActive(true);
                     return;
                 }
-                money -= firstItemPrice;
-                confirmScreen.SetActive(false);
-            }
-            public void ConfirmSecound() {
-                if (money < secondItemPrice)
-                {
-                    poorScreen.SetActive(true);
-                    return;
-                }
-                money -= secondItemPrice;
-                confirmScreen.SetActive(false);
-            }
-            public void ConfirmThird() {
-                if (money < thirdItemPrice)
-                {
-                    poorScreen.SetActive(true);
-                    return;
-                }
-                money -= thirdItemPrice;
-                confirmScreen.SetActive(false);
-            }
-
-
-        public void Cancel() { 
-            confirmScreen.SetActive(false);
-        }
-
-        public void Continue(){
-            confirmScreen.SetActive(false);
-            poorScreen.SetActive(false);
-        }
-
-        public void Buy() { 
-            if(confirmScreen.activeSelf == true) { 
+                itemSlot = 0;
                 confirmScreen.SetActive(true);
             }
+            public void OnSecound() {
+                if (money < secondItemPrice){
+                    poorScreen.SetActive(true);
+                    return;
+                }
+                itemSlot = 1;
+                confirmScreen.SetActive(true);
+            }
+            public void OnThird() {
+                if (money < thirdItemPrice){
+                    poorScreen.SetActive(true);
+                    return;
+                }
+                itemSlot = 2;
+                confirmScreen.SetActive(true);
+            }
+            public bool ConfirmCheck(int price){ 
+                money -= price;
+                confirmScreen.SetActive(false);
+                return true; 
+            }
+            public void OnDeny() { 
+                confirmScreen.SetActive(false);
+            }
 
-        }
+            public void OnConfirm(){
+                if(itemSlot == 0){
+                    ConfirmCheck(firstItemPrice);
+                }else if (itemSlot == 1) { 
+                    ConfirmCheck(secondItemPrice);
+                }else if(itemSlot == 2) {
+                    ConfirmCheck(thirdItemPrice);       
+                }
+                itemSlot = 4;
+            }
 
     //Unity Methods
         private void Start(){
-            Button closeTab = closeButton.GetComponent<Button>();
-            Button firstTab = firstButton.GetComponent<Button>();
-            Button secoundTab = secoundButton.GetComponent<Button>();
-            Button thirdTab = thirdButton.GetComponent<Button>();
-            closeTab.onClick.AddListener(OnCloseClick);
-            firstTab.onClick.AddListener(ConfirmFirst);
-            secoundTab.onClick.AddListener(ConfirmSecound);
-            thirdTab.onClick.AddListener(ConfirmThird);
+            //Button Starters
+                Button closeTab = closeButton.GetComponent<Button>();
+                Button firstTab = firstButton.GetComponent<Button>();
+                Button secoundTab = secoundButton.GetComponent<Button>();
+                Button thirdTab = thirdButton.GetComponent<Button>();
+                Button confirmTab = confirmButton.GetComponent<Button>();
+                Button denyTab = denyButton.GetComponent<Button>();
+                closeTab.onClick.AddListener(OnCloseClick);
+                firstTab.onClick.AddListener(OnFirst);
+                secoundTab.onClick.AddListener(OnSecound);
+                thirdTab.onClick.AddListener(OnThird);
+                confirmTab.onClick.AddListener(OnConfirm);
+                denyTab.onClick.AddListener(OnDeny);
+            //Item Startres
+                firstItemText.SetText($"{firstItemName} : {firstItemPrice}");
+                secondItemText.SetText($"{secondItemName} : {secondItemPrice}");
+                thirdItemText.SetText($"{thirdItemName} : {thirdItemPrice}");
+                firstGameObject.GetComponent<Image>().sprite = firstSprite;
+                secondGameObject.GetComponent<Image>().sprite = secondSprite;
+                thirdGameObject.GetComponent<Image>().sprite = thirdSprite;
     }
         private void Update(){
             if (BuildInputHandler.isInteracting && ison){
--- Assets/Build 26/Resources/Script/WheelScripts/WheelHandler.cs
+++ Assets/Build 26/Resources/Script/WheelScripts/WheelHandler.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WheelHandler : MonoBehaviour{
     //Variables
     #region
     [SerializeField] private bool inTrigger;
     [SerializeField] private SceneHandler sceneHandler;
+    [SerializeField] private Scene scene;
     #endregion
 
     //Methdos
@@ -20,7 +22,13 @@ public class WheelHandler : MonoBehaviour{
     //Scene Methdos
     public void ChangeScene(){
         sceneHandler = new SceneHandler();
-        sceneHandler.LoadScene("MapScreen");
+        scene = SceneManager.GetActiveScene();
+        if (scene.name == "GameScreen"){
+            sceneHandler.LoadScene("MapScreen");
+        }
+        if (scene.name == "MapScreen"){
+            sceneHandler.LoadScene("GameScreen");
+        }
     }
     //Unity Methdos
     public void Update() {
