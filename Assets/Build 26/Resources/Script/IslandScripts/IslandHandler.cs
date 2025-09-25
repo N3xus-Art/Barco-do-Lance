cd63755de2bdf3ad8d1cfc8080788139929e541e
jprvivasGS <jprvivas@gmail.com>
2025-09-25 12:06:42 -0300

    FIX: parcialmente implementadas as lojas por UI

--- Assets/Build 26/Resources/Script/IslandScripts/IslandHandler.cs
+++ Assets/Build 26/Resources/Script/IslandScripts/IslandHandler.cs
@@ -1,40 +1,118 @@
+using NUnit.Framework.Constraints;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IslandHandler : MonoBehaviour{
-    [SerializeField] private string Index;
+    //Variables
+    #region
+    [Header("----Variaveis de Trigger----")]
     [SerializeField] private bool ison;
-    [SerializeField] private GameObject moneyGO;
-    [SerializeField] private TMP_Text moneytext;
-    SceneController sceneController;
-    void OnTriggerEnter2D(Collider2D other){
-        Index = this.gameObject.name;
-        Debug.Log(Index);
-        Debug.Log(BuildInputHandler.isInteracting);
-        if (BuildInputHandler.isInteracting){
-            Time.timeScale = 0;
-            sceneController = new SceneController();
-            sceneController.LoadScene(Index, LoadSceneMode.Additive);
+    [Header("----Variaveis de Canva----")]
+    [SerializeField] private GameObject shopGO;
+    [SerializeField] private GameObject normalScreen;
+    [SerializeField] private GameObject confirmScreen;
+    [SerializeField] private GameObject poorScreen;
+    [SerializeField] private Button closeButton;
+    [Header("----Variaveis de Items----")]
+    [SerializeField] private TMP_Text moneyText;
+    [SerializeField] private int money;
+    [SerializeField] private int firstItemPrice;
+    [SerializeField] private int secondItemPrice;
+    [SerializeField] private int thirdItemPrice;
+    [SerializeField] private string firstItemName;
+    [SerializeField] private string secondItemName;
+    [SerializeField] private string thirdItemName;
+    [SerializeField] private Button firstButton;
+    [SerializeField] private Button secoundButton;
+    [SerializeField] private Button thirdButton;
+    #endregion
+    //Methods
+    #region
+    //Trigger Methods
+    void OnTriggerEnter2D(Collider2D collider2D){
             ison = true;
         }
-    }
 
-    void OnTriggerExit2D(Collider2D other){
-        Index = "";
-        ison = false;
-
-    }
-    private void Start(){
-            moneyGO = GameObject.FindGameObjectWithTag("ShopMoneyText");
-            moneytext = moneyGO.GetComponent<TMP_Text>();    
-    }
-    private void Update(){
-        Debug.Log(moneyGO);
-        Debug.Log(moneytext);
-        if (ison){
-            moneytext.SetText("Dinheiro: " + BuildInputHandler.playerMoney);
+        void OnTriggerExit2D(Collider2D collider2D){
+            ison = false;
         }
-    }
 
+    //Button Methods
+        public void OnCloseClick(){
+            ison = false;
+            shopGO.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        //Confirms
+            public void ConfirmFirst() {
+                if (money < firstItemPrice){
+                    poorScreen.SetActive(true);
+                    return;
+                }
+                money -= firstItemPrice;
+                confirmScreen.SetActive(false);
+            }
+            public void ConfirmSecound() {
+                if (money < secondItemPrice)
+                {
+                    poorScreen.SetActive(true);
+                    return;
+                }
+                money -= secondItemPrice;
+                confirmScreen.SetActive(false);
+            }
+            public void ConfirmThird() {
+                if (money < thirdItemPrice)
+                {
+                    poorScreen.SetActive(true);
+                    return;
+                }
+                money -= thirdItemPrice;
+                confirmScreen.SetActive(false);
+            }
+
+
+        public void Cancel() { 
+            confirmScreen.SetActive(false);
+        }
+
+        public void Continue(){
+            confirmScreen.SetActive(false);
+            poorScreen.SetActive(false);
+        }
+
+        public void Buy() { 
+            if(confirmScreen.activeSelf == true) { 
+                confirmScreen.SetActive(true);
+            }
+
+        }
+
+    //Unity Methods
+        private void Start(){
+            Button closeTab = closeButton.GetComponent<Button>();
+            Button firstTab = firstButton.GetComponent<Button>();
+            Button secoundTab = secoundButton.GetComponent<Button>();
+            Button thirdTab = thirdButton.GetComponent<Button>();
+            closeTab.onClick.AddListener(OnCloseClick);
+            firstTab.onClick.AddListener(ConfirmFirst);
+            secoundTab.onClick.AddListener(ConfirmSecound);
+            thirdTab.onClick.AddListener(ConfirmThird);
+    }
+        private void Update(){
+            if (BuildInputHandler.isInteracting && ison){
+                Time.timeScale = 0;
+                shopGO.SetActive(true);
+                normalScreen.SetActive(true);
+            }
+            moneyText.SetText("Dinheiro: " + money);
+            if (ison == false){
+                shopGO.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    #endregion
 }
