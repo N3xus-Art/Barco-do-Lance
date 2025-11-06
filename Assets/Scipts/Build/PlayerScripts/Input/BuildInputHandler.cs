b7c437eb5bd4862bdd4da8db1d550fc3c445ebf1
jprvivasGS <62661171+jprvivasGS@users.noreply.github.com>
2025-11-06 14:45:07 -0300

    FEAT: temporario

--- Assets/Scipts/Build/PlayerScripts/Input/BuildInputHandler.cs
+++ Assets/Scipts/Build/PlayerScripts/Input/BuildInputHandler.cs
@@ -53,7 +53,6 @@ public class BuildInputHandler : MonoBehaviour{
         if(context.phase == InputActionPhase.Started){
             isStarted = true;
         }else if (context.phase == InputActionPhase.Performed){
-<<<<<<< Updated upstream
             isInteracting = !isInteracting;
             if (isInteracting){
                 var interagiveis = FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None)
@@ -67,9 +66,7 @@ public class BuildInputHandler : MonoBehaviour{
 
                 }
             }
-=======
             isInteracting = true;
->>>>>>> Stashed changes
         }else if (context.phase == InputActionPhase.Canceled){
             isInteracting = false;
         }
