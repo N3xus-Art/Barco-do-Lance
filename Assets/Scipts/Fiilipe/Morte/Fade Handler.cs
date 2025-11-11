912c8acea7ea3568c9823683a52ddc060c5b33e8
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-11-10 22:04:48 -0300

    FEAT: fade in e out e morte

--- Assets/Scipts/Fiilipe/Morte/Fade Handler.cs
+++ Assets/Scipts/Fiilipe/Morte/Fade Handler.cs
@@ -1,46 +1,56 @@
+using System.Collections;
 using UnityEngine;
 
 public class FadeHandler : MonoBehaviour
 {
-    [SerializeField] private GameObject Player;
-    [SerializeField] private PlayerHandler _PlayerHandler;
-    [SerializeField] private CanvasGroup FadeImage;
-    [SerializeField] private float Speed;
+    [SerializeField] private PlayerHandler _PlayerHander;
+    [SerializeField] private CanvasGroup _CanvasGroup;
+    [SerializeField] private float FadeDuration = 5.0f;
 
-    public void Fade(bool fade)
-    {
-        if (fade)
-        {
-            if (FadeImage.alpha < 1)
-            {
+    public void FadeOut(){
 
-                FadeImage.alpha += Speed * Time.deltaTime;
-
-            }
-            else
-            {
-
-                _PlayerHandler.Morrer();
-
-            }
-        }
-        else
-        {
-
-            if (FadeImage.alpha > 0)
-            {
-
-                FadeImage.alpha -= Speed * Time.deltaTime;
-
-            }
-            else
-            {
-
-
-            }
-
-        }
+        StartCoroutine(Fade(_CanvasGroup, _CanvasGroup.alpha, 1, FadeDuration));
 
     }
 
+    public void FadeIn() { 
+    
+        StartCoroutine(Fade(_CanvasGroup, _CanvasGroup.alpha, 0, FadeDuration));
+
+    }
+
+    public void StopFade()
+    {
+
+        StopAllCoroutines();
+
+    }
+
+    private IEnumerator Fade(CanvasGroup cg, float start, float end, float durantion)
+    {
+
+        float ElapsedTime = 0.0f;
+
+        while (ElapsedTime < durantion)
+        {
+
+            ElapsedTime += Time.deltaTime;
+            cg.alpha = Mathf.Lerp(start, end, ElapsedTime / durantion);
+            yield return null;
+
+        }
+        cg.alpha = end;
+
+        if (end == 1)
+        {
+            _PlayerHander.Morrer();
+
+        }
+
+        if (end == 0) { 
+        
+            _PlayerHander.Spawn();
+        
+        }
+    }
 }
--- Assets/Scipts/Fiilipe/Morte/Player Handler.cs
+++ Assets/Scipts/Fiilipe/Morte/Player Handler.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour
@@ -6,26 +8,38 @@ public class PlayerHandler : MonoBehaviour
     [SerializeField] private float CurrentO2;
     [SerializeField] private FadeHandler _FadeHandler;
     [SerializeField] private MoveTester _MoveTester;
+    [SerializeField] private Transform _Transform;
     [SerializeField] private bool Dead;
     [SerializeField] private float Speed;
+    [SerializeField] private TMP_Text _Text;
 
     private void Awake()
     {
         _MoveTester = gameObject.GetComponent<MoveTester>();
+        _Transform =  gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
         if (CurrentO2 > 0)
         {
-
+            // diminui o o2
             CurrentO2 -= Speed * Time.deltaTime;
 
+            // checa se o O2 acabou
+            if (CurrentO2 <= 0)
+            {
+                //toca a animação de fade
+                _FadeHandler.FadeOut();
+
+            }
         }
-        else
+
+
+        if (Dead)
         {
 
-            _FadeHandler.Fade(true);
+            Recover();
 
         }
         
@@ -33,8 +47,51 @@ public class PlayerHandler : MonoBehaviour
 
     public void Morrer()
     {
-        _MoveTester.enabled = false;
-        Dead = true;
+
+        StartCoroutine(Die());
 
     }
+
+    public void Spawn() { 
+    
+        //reabalita as funções
+        _MoveTester.enabled = true;
+        CurrentO2 = MaxO2;
+        Dead = false;
+    
+    }
+
+    public void Recover()
+    {
+        CurrentO2 = MaxO2;
+        _FadeHandler.StopFade();
+
+    }
+
+
+    private IEnumerator Die()
+    {
+
+        // desabilita a o movimento
+        _MoveTester.enabled = false;
+
+        // move o player pro inicio
+        _Transform.position = Vector3.zero;
+
+        Dead = true;
+
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        _Text.enabled = true;
+
+        yield return new WaitForSecondsRealtime(4f);
+
+        _Text.enabled = false;
+
+        yield return new WaitForSecondsRealtime(0.5f);
+
+        _FadeHandler.FadeIn();
+
+    }
+
 }
